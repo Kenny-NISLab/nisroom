d2b206f4a503bb6ac325dae5b533d77185a6a4a6
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-04-22 01:53:24 +0900

    Develop components

---
+++ components/Student.vue
@@ -0,0 +1,49 @@
+<template>
+  <article class="w-16 md:w-20 mx-3 md:mx-10">
+    <button @click="toggleStatus()">
+      <img
+        src="../assets/images/test.jpg"
+        alt="test"
+        class="border-4 rounded-xl border-gray-400"
+        :class="isActive"
+      />
+    </button>
+    <h2 class="text-xs md:text-base text-center">{{ name }}</h2>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  computed: {
+    isActive() {
+      return {
+        'border-green-400': this.status,
+      }
+    },
+    localStatus: {
+      get() {
+        return this.status
+      },
+      set(value) {
+        this.$emit('update:status', value)
+      },
+    },
+  },
+  methods: {
+    toggleStatus() {
+      this.localStatus = !this.localStatus
+    },
+  },
+}
+</script>
---
+++ components/Students.vue
@@ -0,0 +1,114 @@
+<template>
+  <section class="h-full flex flex-wrap justify-center content-around">
+    <template v-for="(student, index) in students">
+      <Student
+        :key="index"
+        :name="student.name"
+        :status.sync="student.status"
+      />
+    </template>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      students: [
+        {
+          name: 'test',
+          status: true,
+        },
+        {
+          name: 'test2',
+          status: true,
+        },
+        {
+          name: 'test3',
+          status: false,
+        },
+        {
+          name: 'test4',
+          status: true,
+        },
+        {
+          name: 'test5',
+          status: false,
+        },
+        {
+          name: 'test',
+          status: true,
+        },
+        {
+          name: 'test2',
+          status: true,
+        },
+        {
+          name: 'test3',
+          status: false,
+        },
+        {
+          name: 'test4',
+          status: true,
+        },
+        {
+          name: 'test5',
+          status: false,
+        },
+        {
+          name: 'test',
+          status: true,
+        },
+        {
+          name: 'test2',
+          status: true,
+        },
+        {
+          name: 'test3',
+          status: false,
+        },
+        {
+          name: 'test4',
+          status: true,
+        },
+        {
+          name: 'test5',
+          status: false,
+        },
+        {
+          name: 'test',
+          status: true,
+        },
+        {
+          name: 'test2',
+          status: true,
+        },
+        {
+          name: 'test3',
+          status: false,
+        },
+        {
+          name: 'test4',
+          status: true,
+        },
+        {
+          name: 'test5',
+          status: false,
+        },
+        {
+          name: 'test',
+          status: true,
+        },
+        {
+          name: 'test2',
+          status: true,
+        },
+        {
+          name: 'test3',
+          status: false,
+        },
+      ],
+    }
+  },
+}
+</script>
