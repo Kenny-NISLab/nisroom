8e85d1b509ec6840ac837962ad6f3c94919ee525
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-04-26 15:00:45 +0900

    Fix updated time position

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,18 @@
 <template>
-  <header class="h-20 flex justify-center items-center">
+  <header class="h-20 flex justify-center items-center flex-col">
     <h1>
       <img src="@/assets/images/NISROOM.png" alt="NISROOM" class="max-h-12" />
     </h1>
+    <p class="mt-2 text-xs">Updated at {{ updatedTime }}</p>
   </header>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    updatedTime() {
+      return this.$store.state.updatedTime
+    },
+  },
+}
 </script>
